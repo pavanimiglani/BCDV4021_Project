44899dde5a8fd3d3baf97acd50dfda0cb4142720
LoChingHei <145512379+LoChingHei@users.noreply.github.com>
2023-10-25 02:11:02 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import WalletDetails from "./pages/WalletDetails";
 import './App.css';
 
 export default function App() {
+  //style for logout button.
   const buttonStyle = {
     position: 'fixed',
     top: '10px',
@@ -23,26 +24,28 @@ export default function App() {
     cursor: 'pointer',
   };
 
+//User information include address and initial amount.
   const user = {
     address: '0xD06284664319742513E2244e5149f93B0914B409',
     amount: 10,
   };
-
+//useState to track on remaining fund in user acount.
   const [walletRemaining, setwalletRemaining] = useState(10);
   const [TransactionsHistory, setTransactions] = useState([
     { TransactionHash: '0x2446f1fd773fbb9f080e67db8765678shgajhe987413cf28a2a4a6da9bh87d6a', Status: 'Success', Timestamp: '2023-09-23T04:0803.172Z', From: '0xD06284664319742513E2244e5149f93B0914B409', To: '0x1342342d53F17d6df2426029cDC7d41e34123423', Value: '10', GasUse: '21000' },
     { TransactionHash: '0x28h866ffd783ffb9f080u6shik8g6a03c7ed7427c9c9413cf28a2a4a6da9b56c', Status: 'Success', Timestamp: '2023-09-29T04:2307.172Z', From: '0xD06284664319742513E2244e5149f93B0914B409', To: '0x736514Db74DbF23e901A60DAEbaE97f8e9b09873', Value: '20', GasUse: '20000' },
-    // Add more transaction data as needed
   ]);
 
+//call in transfer page to update transaction history and remaining funds in wallet.
   const updateTransactionsHistory = (newTransaction) => {
     setTransactions((prevTransactions) => [...prevTransactions, newTransaction]);
   };
   const updateWalletAmount = (used) =>{
     setwalletRemaining(walletRemaining-used)
   }
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+//Track login state
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const handleLogin = () => {
     setIsLoggedIn(true);
   };
